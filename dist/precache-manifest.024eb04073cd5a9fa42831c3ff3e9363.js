1f692a494178fb7c6248d5372e8361c205add47d
Yawenina <xiaoywkate@163.com>
2018-04-30 22:35:24 +0800

    add dist

---
+++ dist/precache-manifest.024eb04073cd5a9fa42831c3ff3e9363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "060a6c22c508195cd74f",
+    "url": "./js/vendor.060a6c22.js"
+  },
+  {
+    "revision": "8d1f202bcfc2d25ffb04",
+    "url": "./js/manifest.8d1f202b.js"
+  },
+  {
+    "revision": "5efbabc95e4aa0d84d84",
+    "url": "./js/app.5efbabc9.js"
+  },
+  {
+    "revision": "96f5c6e84d9bcba010e33b7827cea1af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5efbabc95e4aa0d84d84",
+    "url": "./css/app.9b8dd09f.css"
+  }
+];
