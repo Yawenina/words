0c4a09610623c019794667bcd804808071710689
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-30 15:58:45 +0000

    Deploy Yawenina/words to github.com/Yawenina/words.git:gh-pages

---
+++ precache-manifest.d676515df2dc00b2f4d39fd8dde983b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "060a6c22c508195cd74f",
+    "url": "./js/vendor.060a6c22.js"
+  },
+  {
+    "revision": "27e3dd7809fdcb1943e5",
+    "url": "./js/manifest.27e3dd78.js"
+  },
+  {
+    "revision": "7b63137cd698f64d7e59",
+    "url": "./js/app.7b63137c.js"
+  },
+  {
+    "revision": "8ff337eb5c70238847919e660e7ee693",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b63137cd698f64d7e59",
+    "url": "./css/app.ad95069b.css"
+  }
+];
